ca771a04a8928a561ae802a587210a5fbd91ed55
Saitabau-Dennis <dennisntete28@gmail.com>
2023-11-22 11:45:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Script for navigation bar
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+/* Script for login & sign up page*/
+
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+loginBtnBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+/*Shopping Cart*/
