7485869b87f96e2bf8e3ce3a8e5681265553fd57
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2023-11-26 10:40:13 +0300

    First commit

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ if (close) {
 }
 
 /* Script for login & sign up page*/
-
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
@@ -25,8 +24,8 @@ const loginBtn = document.getElementById("login");
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
 });
-loginBtnBtn.addEventListener("click", () => {
+
+loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
-
 /*Shopping Cart*/
